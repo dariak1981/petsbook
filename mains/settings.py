d9db073afb3d36e2c192374d2299512f0ae39205
Daria K <kda.cnc@gmail.com>
2020-04-22 23:47:09 +0300

    lsdkjf

--- mains/settings.py
+++ mains/settings.py
@@ -162,6 +162,15 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
+
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+
 CORS_REPLACE_HTTPS_REFERER      = False
 HOST_SCHEME                     = "http://"
 SECURE_PROXY_SSL_HEADER         = None
@@ -171,9 +180,3 @@ CSRF_COOKIE_SECURE              = False
 SECURE_HSTS_SECONDS             = None
 SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
 SECURE_FRAME_DENY               = False
-
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
