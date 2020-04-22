27aba31e77d3ffa5b9fc3573cd087026e0e09c8f
Daria K <kda.cnc@gmail.com>
2020-04-22 16:44:25 +0300

    settings

--- mains/settings.py
+++ mains/settings.py
@@ -152,8 +152,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-
-
 LOGIN_REDIRECT_URL = '/users/profile'
 LOGIN_URL = 'login'
 
@@ -162,3 +160,13 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
