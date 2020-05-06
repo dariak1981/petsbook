174b80bdcd4cb7d7093b91f022bb2550e3dcb1b3
Daria K <kda.cnc@gmail.com>
2020-05-06 08:55:18 +0300

    meta ie

--- mains/settings.py
+++ mains/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9&)_r+f^3rgyn-57zx6xz%)a5xe(oj#7+x55&qo=*xnnq@$q19'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 DEBUG = False
 
 ALLOWED_HOSTS=['*']
