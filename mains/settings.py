3747173bdac31fa0ce7c8c341abeb06653f4944b
Daria K <kda.cnc@gmail.com>
2020-06-18 23:55:26 +0300

    contrib admin

--- mains/settings.py
+++ mains/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'accounts',
     'blog.apps.BlogConfig',
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
